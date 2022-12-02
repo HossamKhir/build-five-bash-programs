5787251275021e0df8d337e301172fc168246865
HossamKhir <h.khair95@outlook.com>
2022-12-02 11:45:52 +0000

    Save progress: 260.1

--- questionnaire.sh
+++ questionnaire.sh
@@ -3,6 +3,7 @@ QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
 echo $QUESTION1
 read NAME
-echo Hello $NAME.
+# echo Hello $NAME.
 echo $QUESTION2
 read LOCATION
+echo Hello $NAME from $LOCATION.
